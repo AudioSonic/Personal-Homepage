9b8e4fa1a8e781ca58179be77df2d05f60262ad8
Alexander Stach <stach.alex@yahoo.de>
2026-05-21 20:18:55 +0200

    Die vorherige Struktur wurde entfernt und durch eine neue ersetzt
    Jetzt sind die ganzen Dateien in verschiedene Ordner aufgeteilt
    Außerdem wurde das gitignore schonmal mit den notwendigsten Daten ausgestattet
    
    #25 #35

--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("ueberMich");
-const text = document.getElementById("ueberMichText");
-
-button.addEventListener("click", () => {
-    text.classList.toggle("active");
-});
