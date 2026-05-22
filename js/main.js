f8f2923bbc9cce40b29c528a5e2f525fe126b09d
Alexander Stach <stach.alex@yahoo.de>
2026-05-22 17:09:28 +0200

    CSS Variablen definiert
    Es wurde  schon mal eine Grundstruktur für die Variablen definiert.
    DIese können später dann ganz bequem angepasst werden.
    
    #29

--- js/main.js
+++
@@ -1,9 +0,0 @@
-async function loadComponent(path, elementId) {
-    const response = await fetch(path);
-    const data = await response.text();
-
-    document.getElementById(elementId).innerHTML = data;
-}
-
-loadComponent("header.html", "header");
-loadComponent("footer.html","footer");
