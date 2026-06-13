1625f63aeff5fd2e2116ab6936d38c2aa2f15317
Alexander Stach <stach.alex@yahoo.de>
2026-06-13 22:48:54 +0200

    Datenmodell für das Skillsystem angelegt
    Es wurde ein Datenmodell angelegt, mit dem künftig getestet werden kann.
    Später sollen die Daten sowieso aus einer json ausgelesen werden, weshalb
    es erst einmal nur simple Platzhalter sind.
    
    #81

--- js/data/skills.js
+++ js/data/skills.js
@@ -1,9 +1,110 @@
-export const skills = [
+const skills = [
+    {
+        id: 1,
+        name: "HTML", 
+        category: "web",
+        description: "Basic HTML", 
+        detailedDescription: "Erstellung semantischer Webseiten mit Fokus auf Struktur, Accessibility und SEO.",
+        image: "assets/images/html.png",
+        skillList: [
+            {
+                name: "Semantisches HTML",
+                completed: true
+            }, 
+            {
+                name: "SEO Optimierung",
+                completed: false
+            }]
+    },  
 
     {
-        title: "HTML",
-        category: "frontend",
-        level: 90
-    }
+        id: 2,
+        name: "CSS", 
+        category: "web",
+        description: "Schöneres HTML", 
+        detailedDescription: "Styling der HTML Seiten auf moderne Art und Weise",
+        image: "assets/images/html.png",
+        skillList: [
+            {
+                name: "Flex-Display",
+                completed: false
+            }, 
+            {
+                name: "Hover-Effekte",
+                completed: true
+            }]
+    },  
+    
+    {
+        id: 3,
+        name: "JavaScript", 
+        category: "web",
+        description: "Funktionales HTML", 
+        detailedDescription: "Hinzufügen von Funktion und Logik auf der Webseite",
+        image: "assets/images/html.png",
+        skillList: [
+            {
+                name: "Schleifen",
+                completed: false
+            }, 
+            {
+                name: "Dynamisches Karussell",
+                completed: false
+            }]
+    },  
 
-];"use strict";
+        {
+        id: 4,
+        name: "C#", 
+        category: "software",
+        description: "Objektorientierte Progammiersprache", 
+        detailedDescription: "Programmiersprache aus dem .NET Framework",
+        image: "assets/images/html.png",
+        skillList: [
+            {
+                name: "Foreach-Schleifen",
+                completed: false
+            }, 
+            {
+                name: "Lambda-Expressions",
+                completed: false
+            }]
+    },  
+
+        {
+        id: 5,
+        name: "WPF", 
+        category: "software",
+        description: "Windows Presentation Foundation", 
+        detailedDescription: ".NET Framework zum erstellen von PC Anwendungen",
+        image: "assets/images/html.png",
+        skillList: [
+            {
+                name: "XAML Layouts",
+                completed: false
+            }, 
+            {
+                name: "Data Binding",
+                completed: false
+            }]
+    },  
+
+        {
+        id: 6,
+        name: "Unity", 
+        category: "gamedev",
+        description: "Game Development Engine", 
+        detailedDescription: "Eine umfangreiche Engine zum erstellen von Spielen, Filmen nd weiteren Medienformen",
+        image: "assets/images/html.png",
+        skillList: [
+            {
+                name: "Unity C#",
+                completed: false
+            }, 
+            {
+                name: "Animator",
+                completed: false
+            }]
+    },  
+    ]
+
